12dd8223a5aa5941753aa8550814519b0ca3c307
Janier <janierf.quinonesq@konradlorenz.edu.co>
2024-09-06 13:21:47 -0500

    Creación de clase principal mas prueba de los métodos

--- src/main/java/com/mycompany/cuenta/bancaria/poo/Principal.java
+++ src/main/java/com/mycompany/cuenta/bancaria/poo/Principal.java
@@ -2,6 +2,17 @@ package com.mycompany.cuenta.bancaria.poo;
 
 public class Principal {
     public static void main(String[] args) {
-        // TODO: Adiciona aquí el código que deseas para la Cuenta Bancaria.
+      CuentaBancaria cuenta = new CuentaBancaria("Janier","34",30000);
+        
+     cuenta.CalcularInteres();
+      cuenta.setTipoInteres(20);
+       cuenta.retirar(0);
+    
+       CuentaBancaria cuenta2 = new CuentaBancaria("Farid","23",90000);
+       
+       cuenta2.CalcularInteres();
+       cuenta2.ingresar(0);
+       cuenta2.setTipoInteres(5);
+       
     }
 }
